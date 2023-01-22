4e35e4a7e0a0a29002e7376e5569b482ded02e21
abdelrahman-coder <abdelrahman.zidan23@gmail.com>
2023-01-22 08:48:19 -0800

    q0

--- functions2.c
+++ functions2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
@@ -25,33 +24,26 @@ int print_pointer(va_list types, char buffer[],
 
 	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
-
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
-
 	num_addrs = (unsigned long)addrs;
-
 	while (num_addrs > 0)
 	{
 		buffer[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
-
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
 		extra_c = '+', length++;
 	else if (flags & F_SPACE)
 		extra_c = ' ', length++;
-
 	ind++;
-
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
 }
-
 /************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
@@ -76,22 +68,17 @@ int print_non_printable(va_list types, char buffer[],
 
 	if (str == NULL)
 		return (write(1, "(null)", 6));
-
 	while (str[i] != '\0')
 	{
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
 			offset += append_hexa_code(str[i], buffer, i + offset);
-
 		i++;
 	}
-
 	buffer[i + offset] = '\0';
-
 	return (write(1, buffer, i + offset));
 }
-
 /************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
@@ -103,7 +90,6 @@ int print_non_printable(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
-
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -116,16 +102,13 @@ int print_reverse(va_list types, char buffer[],
 	UNUSED(size);
 
 	str = va_arg(types, char *);
-
 	if (str == NULL)
 	{
 		UNUSED(precision);
-
 		str = ")Null(";
 	}
 	for (i = 0; str[i]; i++)
 		;
-
 	for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
@@ -157,6 +140,7 @@ int print_rot13string(va_list types, char buffer[],
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	str = va_arg(types, char *);
+
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
